ebdec31e8e43fc1d7a7c66329d5057e44abaa1f0
Luis Perez <luis.perez@bluewolf.com.mx>
2019-10-14 12:12:51 -0600

    Create instaladores.php

---
+++ instaladores.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Zodiac - Instaladores</title>
+        <link rel="shortcut icon" href="public/img/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css"/>
+        <link rel="stylesheet" href="public/css/bootstrap/bootstrap-theme.min.css"/>
+        <link rel="stylesheet" href="public/css/font-awesome/css/font-awesome.min.css"/>
+        <link rel="stylesheet" href="public/css/metisMenu/metisMenu.min.css"/>
+        <link rel="stylesheet" href="public/css/sb-admin-2/sb-admin-2.css"/>
+        <link rel="stylesheet" href="public/css/jqueryui/jquery-ui.min.css">
+        <link rel="stylesheet" href="public/css/jqueryui/jquery-ui.structure.min.css">
+        <link rel="stylesheet" href="public/css/jqueryui/jquery-ui.theme.min.css">
+        <link rel="stylesheet" href="public/css/style.css"/>
+        <link rel="stylesheet" href="public/css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" charset="utf-8" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css" rel="stylesheet" />
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>	 
+        <script src="colorbox/jquery.colorbox-min.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="colorbox/colorbox.css" />
+        <link rel="stylesheet" href="pop.css"/>
+        <script type="text/javascript" src="js/cortes_p.js"></script>
+        <style type="text/css">
+            .style1 
+            {
+                color: #FFFFFF;
+            }
+        </style>   
+<body>
+    <ul>
+        <li><a href="instaladores/httpd-2.2.22-win32-x86-no_ssl.msi">apache 2.2.22</a></li>
+        <li><a href="instaladores/httpd-2.2.25-win32-x86-no_ssl.msi">apache 2.2.25</a></li>
+        <li><a href="instaladores/mysql-5.6.45-win32.zip">mysql 5.6.45 win 32</a></li>
+        <li><a href="instaladores/mysql-5.6.45-winx64.zip">mysql 5.6.45 win 64</a></li>
+        <li><a href="instaladores/php-7.3.9-nts-Win32-VC15-x64.zip">php 7 win 64</a></li>
+    </ul>
+    
+    
+    
+    
+</body>
+</html>
